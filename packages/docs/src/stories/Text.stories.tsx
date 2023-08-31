edb7340462cd66cad1d8b284612da475e7eae20c
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-08-30 22:35:38 -0300

    added box & text Components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sapiente animi eius modi beatae, vel iusto architecto deserunt magnam sint non deleniti numquam blanditiis sed tempora alias debitis esse impedit?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
